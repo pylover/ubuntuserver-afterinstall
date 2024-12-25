30ccfaa74857ae620d347d4ec4d8183fd89d480d
pylover <vahid.mardani@gmail.com>
2024-12-25 21:29:08 +0330

    Feature: restart ssh

--- do.sh
+++ do.sh
@@ -48,11 +48,10 @@ sudoer_create () {
   keyfile=$2
 
   echo "creating user: ${username} ..."
-  adduser ${username} && \
-  adduser ${username} sudo || {
-    return 1
-  }
+  adduser ${username}
+  adduser ${username} sudo
 
+  # ssh public key file
   if [ -n "${keyfile}" ]; then
     sshdir=/home/${username}/.ssh
     if [ ! -d $sshdir ]; then
@@ -64,10 +63,16 @@ sudoer_create () {
     chown ${username}:${username} $sshdir/authorized_keys
     chmod 600 $sshdir/authorized_keys
   fi
-
+ 
+  # superuser
   if [[ "${username}" =~ "${SUPERUSER}" ]]; then
     echo "${username} ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/${username}"
     chmod 0440 "/etc/sudoers.d/${username}"
+  fi
+
+  read -p "Do you want to enable vi editing mode for ${username}? [N/y] " 
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    inputrc_set_vimode ${username} 
   fi
 }
 
@@ -90,7 +95,7 @@ sudoerkeys_createall () {
     username="${BASH_REMATCH[1]}"
     read -p "Do you want to create the ${username} user? [N/y] " 
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      sudoer_create ${username} $keyfile
+      sudoer_create ${username} ${keyfile}
     fi
   done
 }
@@ -153,7 +158,6 @@ fi
 # editor -- vim
 read -p "Do you want to install VIM and set it as the default editor? [N/y] " 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  usevim=yes
   apt-get install -y vim
 
   # shell default editor
@@ -162,7 +166,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   fi
 
   # shell vi input mode
-  inputrc_set_vimode /root
+  read -p "Do you want to enable vi editing mode for root? [N/y] " 
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    inputrc_set_vimode /root
+  fi
 fi
 
 
@@ -227,103 +234,18 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
       continue
     }
 
-    sudoer_create $adminuser
+    sudoer_create ${adminuser}
     break;
   done
 fi
 
 
-########################################################
-echo "Under construction....."
+if [[ "${sshrestart}" == "yes" ]]; then
+  read -p "Do you want to restart the SSH server? [N/y] " 
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    systemctl restart ssh
+  fi
+fi
 
 
-# echo "Restarting SSH service..."
-# systemctl restart sshd
-# if [ -z "${SSH_PORT}" ]; then
-#   echo "SSH port [Enter=1111]:"
-#   read input_ssh_port
-#   if [ -z "${input_ssh_port}" ]; then
-#     SSH_PORT="1111"
-#   else
-#     SSH_PORT="${input_ssh_port}"
-#   fi
-# fi
-
-
-################################################################
-# ROLLBACK_TIMER=30
-# SCREEN_NAME="iptables_restore"
-# SERVER_IP=$(hostname -I | awk '{print $1}')
-# 
-# 
-# # Back up current iptables configuration
-# echo "Backing up current iptables rules..."
-# mkdir -p /etc/iptables/
-# iptables-save > /etc/iptables/rules.v4.backup
-# 
-# 
-# # Rollback script file
-# ROLLBACK_FILE="/tmp/firewall_rollback.sh"
-# echo '#!/usr/bin/env bash
-# 
-# echo "Applying firewall rollback..."
-# iptables-restore < /etc/iptables/rules.v4.backup
-# ' > ${ROLLBACK_FILE}
-# chmod +x "${ROLLBACK_FILE}"
-# 
-# 
-# # Start a screen session to ensure rollback can be applied if disconnected.
-# # If no confirmation is provided by the user (or user disconnects), after $ROLLBACK_TIMER seconds rollback is triggered.
-# screen -dmS ${SCREEN_NAME} bash -c "sleep ${ROLLBACK_TIMER} && bash ${ROLLBACK_FILE} && echo 'Firewall rollback applied due to timeout.'"
-# 
-# 
-# # Apply new iptables rules
-# echo "Applying new iptables firewall rules..."
-# iptables -X
-# iptables -P INPUT ACCEPT
-# iptables -P OUTPUT ACCEPT
-# iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables -A INPUT -s 127.0.0.0/8 -j ACCEPT
-# iptables -A INPUT -d $SERVER_IP/32 -p tcp -m tcp --sport 1024:65535 --dport ${SSH_PORT} -j ACCEPT  # SSH access
-# iptables -A INPUT -d $SERVER_IP/32 -p tcp -m tcp --sport 1024:65535 --dport 80 -j ACCEPT           # HTTP
-# iptables -A INPUT -d $SERVER_IP/32 -p tcp -m tcp --sport 1024:65535 --dport 443 -j ACCEPT          # HTTPS
-# iptables -P FORWARD DROP
-# iptables -P INPUT DROP
-# 
-# 
-# # Save iptables rules (temporarily, final save will be after confirmation)
-# iptables-save > /etc/iptables/rules.v4
-# 
-# 
-# # Ask user for confirmation with timeout
-# echo "Do you have access to the server now (y/n)? If you do not confirm within ${ROLLBACK_TIMER} seconds, rollback will occur:"
-# if ! read -t ${ROLLBACK_TIMER} user_response; then
-#   user_response="n"
-# fi
-# 
-# 
-# if [[ "$user_response" == "y" ]]; then
-#   echo "User confirmed access. Canceling rollback..."
-#   # Stop the rollback screen session
-#   screen -S ${SCREEN_NAME} -X quit &>/dev/null || true
-#   # Remove rollback file
-#   rm -f "${ROLLBACK_FILE}"
-#   # Persist new rules
-#   iptables-save > /etc/iptables/rules.v4
-#   echo "Iptables rules confirmed and saved permanently."
-# else
-#   bash "${ROLLBACK_FILE}"
-#   # Clean up rollback screen session if still running
-#   screen -S ${SCREEN_NAME} -X quit &>/dev/null || true
-#   err "No confirmation received. Rolling back firewall changes..."
-#   exit 1
-# fi
-# 
-# 
-# # Clean up if still needed (no harm if already cleaned)
-# screen -S ${SCREEN_NAME} -X quit &>/dev/null || true
-# 
-# 
-# 
-# 
-# echo "Server initialization completed successfully."
+echo "Server configuration has been completed successfully."

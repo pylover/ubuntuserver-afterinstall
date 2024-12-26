bbe9d2c2856d5401faeee1e7cfb8d419440e81a8
pylover <vahid.mardani@gmail.com>
2024-12-27 00:08:12 +0330

    Simplified

--- TODO.md
+++
@@ -1,2 +0,0 @@
-- sshport
-- configure firewall
--- do.sh
+++ do.sh
@@ -7,6 +7,8 @@ publicip=$(hostname -I | awk '{print $1}')
 userpat="[a-z]{3,}"
 pubfilepat="($userpat)\.pub"
 rollbacktout=30
+iptbackupfile=/etc/iptables/rules.v4.back
+screenid=iptrollback
 
 
 err () {
@@ -105,6 +107,51 @@ sudoerkeys_createall () {
 }
 
 
+ipt () {
+  echo "--> iptables $@"
+  iptables $@ 
+  return $?
+}
+
+
+ipt_accept () {
+  ipt -C $@ -jACCEPT 2>/dev/null || ipt -A $@ -jACCEPT 
+  return $?
+}
+
+
+ipt_accept_input () {
+  ipt_accept INPUT $@
+  return $?
+}
+
+
+ipt_accept_input_tcp () {
+  ipt_accept_input -d${publicip}/32 -ptcp -mtcp --sport 1024:65535 $@
+  return $?
+}
+
+
+bgrollbacktask_start () {
+  local cmd
+
+  cmd="sleep $1"
+  cmd="${cmd} && iptables-restore < ${iptbackupfile}"
+  cmd="${cmd} && iptables -P INPUT ACCEPT"
+  cmd="${cmd} && echo 'Firewall rules has been rollbacked due the timeout.'"
+  
+  # start a screen session to ensure rollback can be applied if disconnected.
+  # if no confirmation is provided by the user (or user disconnects), 
+  # after a few seconds rollback is triggered.
+  echo -- screen -dmS ${screenid} bash -c "${cmd}"
+  screen -dmS ${screenid} bash -c "${cmd}"
+}
+
+
+bgrollbacktask_kill () {
+  # Stop the rollback screen session
+  screen -S ${screenid} -X quit
+}
 # check the platform
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "Platform: $OSTYPE"
@@ -273,8 +320,6 @@ fi
 # firewall configration0
 read -p "Do you want to configure iptables? [N/y] " 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  source ${here}/ipt.sh
-  
   # installing required packages
   apt-get install -y iptables-persistent
 
